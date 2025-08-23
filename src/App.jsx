9cf973f94a61308b2bcd7d134fc4b0f94d1cf5b0
DemiGod <demigod14100@gmail.com>
2025-08-23 14:50:58 +0530

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,34 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Angelwing from "../public/Angelwing";
 import Earth2 from "../public/Earth2";
+import Dither from "../public/src/blocks/Backgrounds/Dither/Dither";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Canvas>
-        <OrbitControls autoRotate enableZoom={false} />
-        <Suspense fallback={null}>
-          <Earth2 position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
-          <Environment preset="sunset" />
-        </Suspense>
-      </Canvas>
+        <Canvas className="model-canvas" >
+          <color attach="background" args={['black']} />
+          <OrbitControls autoRotate enableZoom={false} />
+          <Suspense fallback={null}>
+            <Earth2 position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
+            <Environment preset="sunset" />
+          </Suspense>
+        </Canvas>
+      
 
-      <Canvas camera={{ position: [0, 0, 15], fov: 50 }}>
-        <OrbitControls enableZoom={false} autoRotate />
-        <ambientLight intensity={1} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <Suspense fallback={null}>
-          <Angelwing position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
-        </Suspense>
-        <Environment preset="city" />
-      </Canvas>
+        <Canvas className="model-canvas" camera={{ position: [0, 0, 15], fov: 50 }}>
+          <color attach="background" args={['black']} />
+          <OrbitControls enableZoom={false} autoRotate />
+          <ambientLight intensity={1} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <Suspense fallback={null}>
+            <Angelwing position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
+          </Suspense>
+          <Environment preset="city" />
+        </Canvas>
+      
     </>
   );
 }
