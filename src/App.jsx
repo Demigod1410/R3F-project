8fdcd39359de0a9aaebc881512e65f2b3ff4cdc7
DemiGod <demigod14100@gmail.com>
2025-08-23 12:12:54 +0530

    Added a new model and added a new canvas for the model with autorotate

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Earth from '../public/Earth'
 import Earth2 from '../public/Earth2'
 import Jupiter from '../public/Jupiter'
 import { Suspense } from 'react'
+import Angelwing from '../public/Angelwing'
 import { Environment, OrbitControls } from '@react-three/drei'
 
 function App() {
@@ -13,12 +14,23 @@ function App() {
 
   return (
     <>
-      <Canvas>
-        <OrbitControls />
+    
+      <Canvas camera={{ position: [0, 0, 15], fov: 50 }}>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={1} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <Suspense fallback={null}>
-          <Earth2 />
+          <Angelwing position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
         </Suspense>
-        <Environment preset="sunset" />
+        <Environment preset="city" />
+      </Canvas>
+    
+    
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth2 position={[0, 0, 0]} scale={1} rotation={[0, 0, 0]} />
+          <Environment preset="sunset" />
+        </Suspense>
       </Canvas>
     </>
   )
